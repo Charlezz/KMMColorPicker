d278d0d533b474bd1b998da6a7d0d5d8cf4fa4d8
ck <dbcjdrb1997@gmail.com>
2023-11-11 02:17:13 +0900

    안드로이드 헥사코드 및 ARGB 변환 기능 추가

---
+++ shared/src/commonMain/kotlin/com/bluestars/colorpicker/model/ColorEnvelope.kt
@@ -0,0 +1,14 @@
+package com.bluestars.colorpicker.model
+
+/**
+ * Data transfer object that includes updated color factors.
+ *
+ * @param color ARGB color value.
+ * @param hexCode Color hex code, which represents [color] value.
+ * @param fromUser Represents this event is triggered by user or not.
+ */
+data class ColorEnvelope(
+    val color : Int,
+    val hexCode : String,
+    val fromUser : Boolean
+)
--- shared/src/commonMain/kotlin/com/bluestars/colorpicker/usecase/Color.kt
+++ shared/src/commonMain/kotlin/com/bluestars/colorpicker/usecase/Color.kt
@@ -19,5 +19,28 @@ object Color {
     fun rgb(red:Int, green:Int, blue:Int):Int{
         return (0xFF000000u or (red.toUInt() shl 16) or (green.toUInt() shl 8) or blue.toUInt()).toInt()
     }
+    fun argb(alpha: Int, red: Int, green: Int, blue: Int): Int {
+        return (alpha and 0xFF shl 24) or (red and 0xFF shl 16) or (green and 0xFF shl 8) or (blue and 0xFF)
+    }
+    fun rgba(red: Int, green: Int, blue: Int, alpha: Int): Int {
+        return argb(alpha, red, green, blue)
+    }
 
+    fun hexCode(red: Float, green: Float, blue: Float, alpha: Float): String {
+        val a = alpha.toHex()
+        val r = red.toHex()
+        val g = green.toHex()
+        val b = blue.toHex()
+        return "$a$r$g$b"
+    }
+
+    private fun Float.toHex(): String {
+        val intValue = (this * 255).toInt()
+        val hexChars = "0123456789ABCDEF"
+
+        return buildString {
+            append(hexChars[(intValue shr 4) and 0x0F])
+            append(hexChars[intValue and 0x0F])
+        }
+    }
 }
